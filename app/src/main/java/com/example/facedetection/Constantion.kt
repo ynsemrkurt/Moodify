b167c859592c73a65ec84b8965cb3740de4f4191
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-09-22 17:17:48 +0300

    Feat: Add genre lists for different moods and Spotify constants

---
+++ app/src/main/java/com/example/facedetection/Constantion.kt
@@ -0,0 +1,22 @@
+package com.example.facedetection
+
+object Mood {
+    const val HAPPY = "Happy"
+    const val SAD = "Sad"
+    const val TIRED = "Tired"
+    const val NEUTRAL = "Neutral"
+    const val MOOD = "Mood"
+}
+
+object Spotify {
+    const val TOKEN_KEY = "Token_Key"
+    const val STREAMING_KEY = "streaming"
+    const val TOKEN_TYPE = "Bearer "
+    const val GENRE_TYPE = "genre:"
+    const val DEF_NAME = "User"
+    const val DEF_TYPE = "Unknown"
+}
+
+object SearchType {
+    const val PLAYLIST = "playlist"
+}
---
+++ app/src/main/java/com/example/facedetection/GenreLists.kt
@@ -0,0 +1,24 @@
+package com.example.facedetection
+
+object GenreLists {
+
+    val happyGenres = listOf(
+        "afrobeat", "bossa nova", "dance", "disco", "funk", "party", "pop",
+        "power-pop", "salsa", "samba", "sertanejo", "tango", "tropical"
+    )
+
+    val sadGenres = listOf(
+        "blues", "emo", "sad", "gospel", "goth", "folk",
+        "singer-songwriter", "rainy-day", "ambient"
+    )
+
+    val tiredGenres = listOf(
+        "acoustic", "chill", "lo-fi", "jazz", "classical", "sleep",
+        "study", "new-age", "minimal-techno", "ambient"
+    )
+
+    val neutralGenres = listOf(
+        "indie", "alternative", "indie-pop", "rock", "classical", "pop",
+        "electronic", "techno", "soundtracks", "synth-pop", "world-music"
+    )
+}
