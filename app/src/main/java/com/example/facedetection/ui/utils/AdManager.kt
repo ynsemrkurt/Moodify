dc649fd9c2cdf205ce3a2bd1a162854ae4b954a1
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-10-13 17:41:12 +0300

    Feat: Implement rewarded ad logic

--- app/src/main/java/com/example/facedetection/ui/utils/AdManager.kt
+++ app/src/main/java/com/example/facedetection/ui/utils/AdManager.kt
@@ -1,7 +1,9 @@
 package com.example.facedetection.ui.utils
 
 import android.app.Activity
+import android.widget.Toast
 import com.example.facedetection.BuildConfig.AD_UNIT_ID
+import com.example.facedetection.R
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.rewarded.RewardedAd
@@ -10,20 +12,25 @@ import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 class AdManager(private val activity: Activity) {
     private var rewardedAd: RewardedAd? = null
 
-    fun loadRewardedAd() {
+    fun loadRewardedAd(onAdLoaded: () -> Unit) {
         val adRequest = AdRequest.Builder().build()
+        Toast.makeText(activity, activity.getString(R.string.ad_loading), Toast.LENGTH_SHORT).show()
         RewardedAd.load(activity, AD_UNIT_ID, adRequest, object : RewardedAdLoadCallback() {
             override fun onAdLoaded(ad: RewardedAd) {
                 rewardedAd = ad
+                showRewardedAd {
+                    onAdLoaded.invoke()
+                }
             }
 
             override fun onAdFailedToLoad(adError: LoadAdError) {
                 rewardedAd = null
+                onAdLoaded.invoke()
             }
         })
     }
 
-    fun showAdIfAvailable(onRewarded: () -> Unit) {
+    private fun showRewardedAd(onRewarded: () -> Unit) {
         rewardedAd?.let { ad ->
             ad.show(activity) {
                 onRewarded.invoke()
